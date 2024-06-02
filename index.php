cf70b0a5e68ce6ebb2913598d17f2fdf6b347a4a
Mehdi <mehdi-yacine.boukabou@supdevinci-edu.fr>
2024-06-02 22:12:41 +0200

    création fichier php

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="design.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <title>Locauto</title>
+</head>
+<body>
+    <nav class="navbar">
+        <div class="logo"><h1>FCmcqueen</h1></div>
+        <ul class="menu">
+            <li><a href="#" class="active">Accueil</a></li>
+            <li><a href="#">Ajouter voiture</a></li>
+            <li><a href="#" class="fa-solid fa-car"></a></li>
+            <li><a href="#" class="fa-solid fa-user-plus"></a></li>
+        </ul>
+    </nav>
+    <section class="contenu">
+        <h1>Ajouter voiture</h1>
+        <p>Ajouter de nouveaux voiture afin d'avoir plus de clients!!merci!!!</p>
+        <button>AJOUTER</button>
+        <img src="image/voiture_sportive.png" alt="honda_civic" class="imageposition">
+    </section>
+
+    <h1 class="produits_titre"> Nos Voitures</h1>
+    <section class="section_produits">
+        <div class="produits">
+            <div class="fiche_produit">
+                <div class="img"><img src="image/honda_civic.png" alt="honda_civic">
+                <div class="marque">Honda</div>
+                <div class="modele">Hatchback</div>
+                <div class="contenu_2">
+                    <div class="prix">300 euros par mois</div>
+                    <button class="modification">modifier</button>
+                </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</body>
+</html>
